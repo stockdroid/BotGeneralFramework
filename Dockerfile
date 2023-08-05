c0d42fabcf23aa493a0f28f7d24537506931d744
Nicola Leone Ciardi <nicolaleone.ciardi@gmail.com>
2023-08-06 00:16:28 +0200

    chore[ docker ]: added tests

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM bitnami/dotnet:7.0.9
 WORKDIR /testing
 
 ENV PATH=$PATH:/usr/share/botgf
+
 CMD chmod +x /usr/share/botgf/botgf
---
+++ Tests/EnvBot/types.d.ts
@@ -0,0 +1,38 @@
+interface Context {
+  [key: string]: any;
+}
+type Middleware = (ctx: Context, next: () => void) => void;
+type Event = string;
+
+interface App {
+  use(middleware: Middleware[]): App;
+  on(event: Event, ...middlewares: Middleware[]): App;
+  trigger(event: Event, ctx: Context): App;
+  register(...bots: any[]): App;
+}
+declare const app: App;
+interface BotInfo {
+  readonly name: string;
+  readonly version: string;
+  readonly author: string;
+  readonly description?: string;
+  readonly license?: string;
+  readonly repository?: string;
+}
+interface PlatformInfo {
+  readonly access: { [key: string]: string };
+  readonly options?: { [key: string]: string };
+}
+interface BotConfig {
+  readonly bot: BotInfo;
+  readonly platforms: { [key: string]: PlatformInfo };
+  readonly options?: { [key: string]: string };
+}
+declare const config: BotConfig;
+interface CLIOptions {
+  readonly Verbose: boolean;
+  readonly Time: boolean;
+  readonly ConfigPath: string;
+  readonly MainModule?: string;
+}
+declare const options: CLIOptions;
